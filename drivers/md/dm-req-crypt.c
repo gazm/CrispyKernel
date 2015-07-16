58d89a38ae9cdefde499bcb3e40b250ca3652314
Zhen Kong <zkong@codeaurora.org>
2015-07-16 00:47:59 -0400

    dm-req-crypt: Remove high priority flag from workqueue
    Remove WQ_HIGHPRI flag for the workqueue. WQ_HIGHPRI and
    WQ_UNBOUND flags cannot be used together.
    
    Acked-by: Baranidharan Muthukumaran <bmuthuku@qti.qualcomm.com>
    Signed-off-by: Zhen Kong <zkong@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Conflicts:
    	drivers/md/dm-req-crypt.c
    
    (cherry picked from commit 267b4499be3679d331b024355565b1adcb9f6db0)

--- drivers/md/dm-req-crypt.c
+++ drivers/md/dm-req-crypt.c
@@ -720,9 +720,8 @@ static int req_crypt_ctr(struct dm_target *ti, unsigned int argc, char **argv)
 	start_sector_orig = tmpll;
 
 	req_crypt_queue = alloc_workqueue("req_cryptd",
-					WQ_NON_REENTRANT |
-					WQ_HIGHPRI |
-					WQ_CPU_INTENSIVE|
+					WQ_UNBOUND |
+					WQ_CPU_INTENSIVE |
 					WQ_MEM_RECLAIM,
 					1);
 	if (!req_crypt_queue) {
